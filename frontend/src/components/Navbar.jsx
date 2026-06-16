77c8aa91e2e8ef9b3b2f6767cbd290285ad19da4
ChristianCFT <toledochristian272@gmail.com>
2026-06-16 16:18:12 -0300

    [ADD] Adicionando componentes react (footer e navbar)

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import logo from "../assets/logos/logo-2.jpeg";
+
+function Navbar(){
+    return(
+        <nav>
+
+            <figure>
+                <img src={logo} alt="Logo TC" />
+                <figcaption>TC Criptomoedas</figcaption>
+            </figure>
+
+            <ul>
+
+                <li><a id="inicio" href=""> Início</a></li>
+                <li><a id="criptoativos" href="">Criptoativos</a></li>
+                <li><a id="add-saldo" href="">Adicionar saldo</a></li>
+                <li><a id="sair" href="">Sair</a></li>
+
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
+
+
