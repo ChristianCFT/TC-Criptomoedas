1fb374d0485fcb3ad231c38765e0bdd6a46cee48
Thiago Amaral <thiagoamaralbae033@gmail.com>
2026-06-17 00:14:26 -0300

    [UPDATE] Atualizando para react página de carteira .jsx e .css

---
+++ frontend/src/pages/Carteira.jsx
@@ -0,0 +1,134 @@
+import moeda from "../assets/images/moeda.jpg"
+import carteira from "../assets/images/carteira.jpg";
+import "../styles/carteira.css"
+function Carteira() {
+    return (
+        <>
+
+            {/* Cabeçalho */}
+            <header>
+
+                <h1>
+                    <span>Nome da Carteira</span>
+                </h1>
+
+                <p>
+                    Gerencie seus ativos e acompanhe seu desempenho
+                </p>
+
+            </header>
+
+
+            {/* Principal */}
+            <main>
+
+
+                {/* Resumo */}
+                <section id="resumo-carteira">
+
+
+                    <div className="card-resumo">
+
+                        <img 
+                            src={moeda} 
+                            alt="Moeda" 
+                        />
+
+                        <div>
+
+                            <h3 className="title-card-resumo">
+                                SALDO TOTAL DA CARTEIRA
+                            </h3>
+
+                            <p className="valor-card-resumo">
+                                R$ <span>7399,99</span>
+                            </p>
+
+                        </div>
+
+                    </div>
+
+
+
+                    <div className="card-resumo">
+
+                        <img 
+                            src={carteira} 
+                            alt="Carteira" 
+                        />
+
+                        <div>
+
+                            <h3 className="title-card-resumo">
+                                QUANTIDADE DE CRIPTOMOEDAS
+                            </h3>
+
+                            <p className="valor-card-resumo">
+                                <span>8</span>
+                            </p>
+
+                            <p id="p-dif">
+                                Ativos diferentes
+                            </p>
+
+                        </div>
+
+                    </div>
+
+
+                </section>
+
+
+
+
+                {/* Ativos */}
+                <section id="ativos">
+
+
+                    {/* Cabeçalho ativos */}
+                    <section id="cabecalho-ativos">
+
+
+                        <div className="conteudo-cabecalho">
+
+                            <h2>
+                                Seus Ativos
+                            </h2>
+
+                            <p>
+                                Veja suas criptomoedas e gerencie suas posições
+                            </p>
+
+                        </div>
+
+
+
+                        <div className="acoes-cabecalho">
+
+                            <button id="btn-comprar-cripto">
+                                Comprar Criptomoeda
+                            </button>
+
+                        </div>
+
+
+                    </section>
+
+
+
+                    {/* Tabela */}
+                    <section id="secao-tabela-ativos">
+
+                    </section>
+
+
+                </section>
+
+
+            </main>
+
+        </>
+    );
+}
+
+export default Carteira;
