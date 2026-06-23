bda3c0ac834dcaa59a58c979b85d87622257b1a1
Thiago Amaral <thiagoamaralbae033@gmail.com>
2026-06-23 10:10:35 -0300

    [ADD] adicionando estilização e estrutura do CardCarteira para lógica react

---
+++ frontend/src/components/CardCarteira.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/cardCarteira.css';
+import Carteira from "../assets/images/carteira.jpg"
+
+function CardCarteira({ titulo, saldo}){
+    return (
+        <div className="card-carteira">
+            <div className="card-header">
+                <img src={Carteira} alt='Carteira'/>
+                <h3 className="titulo-carteira">{titulo}</h3>
+            </div>
+            <div className="info-saldo">
+                <p className="titulo-saldo">Saldo total</p>
+                <p className="saldo">R$ <span className="valor-saldo">{saldo}</span></p>
+            </div>
+            <button className="btn-ver-carteira" >Ver carteira</button>
+        </div>
+    );
+};
+
+export default CardCarteira;
