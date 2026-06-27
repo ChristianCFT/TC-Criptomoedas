2d967f39692f9c211fdf0bd65a2a102bd7ab258c
Thiago Amaral <thiagoamaralbae033@gmail.com>
2026-06-27 15:49:32 -0300

    [FIX] arrumando estrutura da pagina carteira

--- frontend/src/pages/Carteira/Carteira.jsx
+++ frontend/src/pages/Carteira/Carteira.jsx
@@ -1,132 +1,132 @@
+import React from 'react';
 import moeda from "@/assets/images/moeda.jpg"
 import carteira from "@/assets/images/carteira.jpg";
+import TabelaAtivos from "@/components/TabelaAtivos/TabelaAtivos";
 import "./carteira.css"
+
 function Carteira() {
+    // exemplo
+   const meusAtivos = [
+    { 
+        id: 1, 
+        imagem: "https://cryptologos.cc/logos/bitcoin-btc-logo.png", // Exemplo com URL
+        nome: "Bitcoin", 
+        sigla: "BTC", 
+        quantidade: 0.025, 
+        preco: "345.000,00",
+        total: "8.625,00"
+    },
+    { 
+        id: 2, 
+        imagem: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
+        nome: "Ethereum", 
+        sigla: "ETH", 
+        quantidade: 1.2, 
+        preco: "18.500,00",
+        total: "22.200,00"
+    },
+    { 
+        id: 1, 
+        imagem: "https://cryptologos.cc/logos/bitcoin-btc-logo.png", // Exemplo com URL
+        nome: "Bitcoin", 
+        sigla: "BTC", 
+        quantidade: 0.025, 
+        preco: "345.000,00",
+        total: "8.625,00"
+    },
+    { 
+        id: 1, 
+        imagem: "https://cryptologos.cc/logos/bitcoin-btc-logo.png", // Exemplo com URL
+        nome: "Bitcoin", 
+        sigla: "BTC", 
+        quantidade: 0.025, 
+        preco: "345.000,00",
+        total: "8.625,00"
+    }
+];
+
     return (
         <>
-
             {/* Cabeçalho */}
             <header className="header-carteira">
-
                 <h1>
                     <span>Nome da Carteira</span>
                 </h1>
-
                 <p>
                     Gerencie seus ativos e acompanhe seu desempenho
                 </p>
-
             </header>
-
 
             {/* Principal */}
             <main className="main-carteira">
 
-
                 {/* Resumo */}
                 <section id="resumo-carteira">
 
-
                     <div className="card-resumo">
-
                         <img 
                             src={moeda} 
                             alt="Moeda" 
                         />
-
                         <div>
-
                             <h3 className="title-card-resumo">
                                 SALDO TOTAL DA CARTEIRA
                             </h3>
-
                             <p className="valor-card-resumo">
                                 R$ <span>7399,99</span>
                             </p>
-
                         </div>
-
                     </div>
 
-
-
                     <div className="card-resumo">
-
                         <img 
                             src={carteira} 
                             alt="Carteira" 
                         />
-
                         <div>
-
                             <h3 className="title-card-resumo">
                                 QUANTIDADE DE CRIPTOMOEDAS
                             </h3>
 
                             <p className="valor-card-resumo">
-                                <span>8</span>
+                                <span>{meusAtivos.length}</span>
                             </p>
-
                             <p id="p-dif">
                                 Ativos diferentes
                             </p>
-
                         </div>
-
                     </div>
 
-
                 </section>
-
-
-
 
                 {/* Ativos */}
                 <section id="ativos">
 
-
                     {/* Cabeçalho ativos */}
                     <section id="cabecalho-ativos">
-
-
                         <div className="conteudo-cabecalho">
-
                             <h2>
                                 Seus Ativos
                             </h2>
-
                             <p>
                                 Veja suas criptomoedas e gerencie suas posições
                             </p>
-
                         </div>
 
-
-
                         <div className="acoes-cabecalho">
-
                             <button id="btn-comprar-cripto">
                                 Comprar Criptomoeda
                             </button>
-
                         </div>
-
-
                     </section>
-
-
 
                     {/* Tabela */}
-                    <section id="secao-tabela-ativos">
-
-                    </section>
-
+                    <TabelaAtivos ativos={meusAtivos} />
+                    
 
                 </section>
 
-
             </main>
-
         </>
     );
 }
