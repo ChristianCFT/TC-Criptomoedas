ea2895878e5b4ff1da9d4201a9f87de7363fa9ee
ChristianCFT <toledochristian272@gmail.com>
2026-07-02 01:16:56 -0300

    [UPDATE] Atualizando os links da barra de navegação para botões e alterando o css para ficar igual ao estilo anterior

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,25 @@
+"use client";
+import { toast } from "sonner";
 import logo from "../../assets/logos/logo-2.jpeg";
+import { logout } from "../../services/auth.services";
 import "./navbar.css"
+import { useRouter } from "next/navigation";
 
 function Navbar(){
+
+    const router = useRouter();
+    const handleLogout = async () =>{
+        try{
+            await logout();
+            toast.success("Logout realizado com sucesso!");
+
+            router.push("/login");
+            router.refresh();
+        } catch(error){
+            toast.error("Erro ao realizar logout!");
+        }
+    }
+
     return(
         <nav className="navbar-container">
 
@@ -12,12 +30,24 @@ function Navbar(){
 
             <ul className="navbar-ul">
 
-                <li className="navbar-li"><a className="navbar-a" id="inicio" href=""> Início</a></li>
-                <li className="navbar-li"><a className="navbar-a" id="criptoativos" href="">Criptoativos</a></li>
-                <li className="navbar-li"><a className="navbar-a" id="add-saldo" href="">Adicionar saldo</a></li>
-                <li className="navbar-li"><a className="navbar-a" id="sair" href="">Sair</a></li>
+                <li className="navbar-li">
+                    <button className="navbar-button" id="inicio">Início</button>
+                </li>
+
+                <li className="navbar-li">
+                    <button className="navbar-button" id="criptoativos">Criptoativos</button>
+                </li>
+
+                <li className="navbar-li">
+                    <button className="navbar-button" id="add-saldo">Adicionar saldo</button>
+                </li>
+
+                <li className="navbar-li">
+                    <button className="navbar-button" id="sair" onClick={handleLogout}>Sair</button>
+                </li>
 
             </ul>
+
         </nav>
     );
 }
