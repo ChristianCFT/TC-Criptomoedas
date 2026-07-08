28c1774fe36e95f38cdee2d42cc95e9f8590c924
ChristianCFT <toledochristian272@gmail.com>
2026-07-07 22:20:18 -0300

    [UPDATE] Modificando arquivo para que o button ver carteira acesse a rota carteira/id

--- frontend/src/components/CardCarteira/CardCarteira.tsx
+++ frontend/src/components/CardCarteira/CardCarteira.tsx
@@ -1,8 +1,13 @@
+"use client";
 import React from 'react';
 import './cardCarteira.css';
 import CarteiraImg from "../../../src/assets/images/carteira.jpg";
+import { useRouter } from 'next/navigation';
 
-function CardCarteira({ titulo, saldo}){
+function CardCarteira({ id, titulo, saldo}){
+
+    const router = useRouter();
+
     return (
         <div className="card-carteira">
             <div className="card-header">
@@ -11,9 +16,9 @@ function CardCarteira({ titulo, saldo}){
             </div>
             <div className="info-saldo">
                 <p className="titulo-saldo">Saldo total</p>
-                <p className="saldo">R$ <span className="valor-saldo">{saldo}</span></p>
+                <p className="saldo">R$ <span className="valor-saldo">{Number(saldo).toFixed(2)}</span></p>
             </div>
-            <button className="btn-ver-carteira" >Ver carteira</button>
+            <button className="btn-ver-carteira" onClick={() => router.push(`/carteira/${id}`)}>Ver carteira</button>
         </div>
     );
 };
