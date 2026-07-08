fb47fb54d91d551c56e4f75c5b93394c2033373f
Thiago Amaral <thiagoamaralbae033@gmail.com>
2026-07-08 17:38:40 -0300

    [UPDATE] atualizando mercado de criptoaticos de visualição

--- frontend/src/components/TabelaMercado/LinhaMercado.tsx
+++ frontend/src/components/TabelaMercado/LinhaMercado.tsx
@@ -1,23 +1,32 @@
-// LinhaMercado.jsx
-import '../TabelaAtivos/linhaAtivo.css'; // Você pode reaproveitar o mesmo CSS da linha da carteira!
-import logo from "../../assets/logos/logo-2.jpeg"
+import '../TabelaAtivos/linhaAtivo.css';
 
-export default function LinhaMercado({ moeda, sigla, preco }) {
+export default function LinhaMercado({ nome, sigla, preco, variacao24h, marketCap, imagemUrl }) {
+    
+    const corVariacao = variacao24h >= 0 ? "cor-positiva" : "cor-negativa";
+    const sinal = variacao24h > 0 ? "+" : "";
+
     return (
         <tr className="linha-ativo">
             <td className="coluna-cripto">
                 <div className="ativo-cripto">
-                    {/* Mantemos a foto e os nomes */}
-                    <img src={logo.src} alt={moeda} />
+                    <img src={imagemUrl} alt={`Logo ${nome}`} />
+                    
                     <div className="info-cripto">
-                        <p className="p-nome-moeda">{moeda}</p>
+                        <p className="p-nome-moeda">{nome}</p>
                         <p className="p-sigla-moeda">{sigla}</p>
                     </div>
                 </div>
             </td>
             
-            {/* Apenas mostramos o preço global da moeda */}
             <td className="coluna-preco">R$ {preco}</td>
+            
+            <td className={corVariacao}>
+                {sinal}{variacao24h}%
+            </td>
+            
+            <td className="coluna-marketcap">
+                R$ {marketCap}
+            </td>
         </tr>
     );
 }
